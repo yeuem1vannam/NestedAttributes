44a0de25e749ad4f1ffe04fa722daff16e9ba314
Phuong 'J' Le H <le.hoai.phuong@framgia.com>
2013-08-17 04:08:27 +0700

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedAttributes::Application.config.secret_token = 'd4f01ea7bbdca83c8e5087d1109f3081d7410956b85bd46b3c33a841a892b4b5d1ce76029d9556192c6c53bb58a22bde741b3be45374779330ca5f2c42800ada'
